48ad9ba8dc52afd8851145810df6afd7f6ab077e
VBTX <jv.svedaite@gmail.com>
2019-10-21 18:41:08 -0500

    add persistor to store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import ToDos from './reducers/ToDos';
-
+import { persistStore } from 'redux-persist';
 const reducer = combineReducers({
 	ToDos
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
-
+export const store = createStore(
+	reducer,
+	composeEnhancer(applyMiddleware(thunk))
+);
+export const persistor = persistStore(store);
 export default store;
