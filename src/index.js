503d4e8768c9d1d0fb266b6cfc7f6f5a772ecc9a
VBTX <jv.svedaite@gmail.com>
2019-10-21 19:15:44 -0500

    add PersistGate wrapper in order to persist certain reducers in local storage

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<PersistGate persistor={persistor}>
+			<App />
+		</PersistGate>
 	</Provider>,
 	document.getElementById('root')
 );
@@ -16,4 +19,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
