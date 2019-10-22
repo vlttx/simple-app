f2373c63b5463c9ff33967cdec357c3a1d1886f7
VBTX <jv.svedaite@gmail.com>
2019-10-21 19:20:45 -0500

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './components/pages/Homepage';
 import Homepage from './components/pages/Homepage';
@@ -9,7 +8,6 @@ class App extends Component {
 		return (
 			<div className='App'>
 				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
 					<Homepage />
 				</header>
 			</div>
