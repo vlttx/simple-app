ea5fd875486909daac8d62a2c8cfe1210df3f004
VBTX <jv.svedaite@gmail.com>
2019-10-21 20:25:46 -0500

    add a condition for display when Item is complete and when not:

--- src/components/todos/ToDoItem.js
+++ src/components/todos/ToDoItem.js
@@ -1,31 +1,76 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { deleteToDo } from '../actions/ToDos';
+import { deleteToDo, completeToDo } from '../actions/ToDos';
 
 class ToDoItem extends Component {
 	constructor(props) {
 		super(props);
-		this.handleSubmit = () => {
+		this.handleDelete = () => {
 			this.props.deleteToDo(this.props.id);
+		};
+		this.handleCheckbox = event => {
+			event.preventDefault();
+
+			const updatedItem = {
+				text: this.props.text,
+				id: this.props.id,
+				complete: true
+			};
+			this.props.completeToDo(updatedItem);
 		};
 	}
 
 	render() {
 		return (
 			<div>
-				<li>
-					{this.props.text}
-					{'   '}
-					<button type='submit' onClick={this.handleSubmit}>
-						Delete
-					</button>
-				</li>
+				{this.props ? (
+					this.props.complete ? (
+						<>
+							<h5
+								style={{
+									textDecorationLine: 'line-through',
+									textDecorationStyle: 'solid'
+								}}
+							>
+								<input
+									type='checkbox'
+									checked={this.props.complete}
+									onChange={this.handleCheckbox}
+								/>
+								{this.props.text}{' '}
+								<button type='submit' onClick={this.handleDelete}>
+									Delete
+								</button>
+							</h5>
+						</>
+					) : (
+						<>
+							<h5>
+								<input
+									type='checkbox'
+									checked={this.props.complete}
+									onChange={this.handleCheckbox}
+								/>
+								{this.props.text}{' '}
+								<button type='submit' onClick={this.handleDelete}>
+									Delete
+								</button>
+							</h5>
+						</>
+					)
+				) : (
+					<h5>"No complete tasks")</h5>
+				)}
 			</div>
 		);
 	}
 }
 
+const mapStateToProps = state => {
+	return { todos: state.todos };
+};
+
 export default connect(
-	null,
-	{ deleteToDo }
+	mapStateToProps,
+	{ deleteToDo, completeToDo }
 )(ToDoItem);
