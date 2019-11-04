0ed073f3c9b82bcabc036d580cecd8ff535e1b5d
VBTX <jv.svedaite@gmail.com>
2019-11-04 08:32:23 -0600

    remove unnecessary styling for now

--- src/components/todos/ToDoContainer.js
+++ src/components/todos/ToDoContainer.js
@@ -5,22 +5,15 @@ import { connect } from 'react-redux';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 
 class ToDoContainer extends Component {
 	render() {
 		return (
-			<MDBContainer>
-				<MDBRow>
-					<MDBCol md='9' className='mb-r'>
-						<h3>To Do List:</h3>
-						<ToDoList items={this.props.items} />
-					</MDBCol>
-					<MDBCol xl='3' md='6' className='mx-auto text-center'>
-						<NewToDo />
-					</MDBCol>
-				</MDBRow>
-			</MDBContainer>
+			<>
+				<h3>To Do List:</h3>
+				<ToDoList items={this.props.items} />
+				<NewToDo />
+			</>
 		);
 	}
 }
