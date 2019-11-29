d061b395896ff82e6f6a4c30a1a3d027ba49821c
VBTX <jv.svedaite@gmail.com>
2019-11-28 21:25:20 -0600

    need to redo

--- src/components/todos/NewToDo.js
+++ src/components/todos/NewToDo.js
@@ -1,44 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { addToDo } from '../actions/ToDos';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import { MDBBtn, MDBCol } from 'mdbreact';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addToDo } from "../actions/ToDos";
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+// import 'bootstrap-css-only/css/bootstrap.min.css';
+// import 'mdbreact/dist/css/mdb.css';
+// import { MDBBtn, MDBCol } from 'mdbreact';
 
 class NewToDo extends Component {
-	state = { text: '', id: null, complete: false };
+  state = { text: "", id: null, complete: false };
 
-	handleChange = e => {
-		this.setState({ text: e.target.value });
-	};
-	handleSubmit = e => {
-		e.preventDefault();
-		const newItem = {
-			text: this.state.text,
-			id: Date.now(),
-			complete: false
-		};
-		this.props.addToDo(newItem);
-		this.setState({ text: '', id: null, complete: false });
-	};
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const newItem = {
+      text: this.state.text,
+      id: Date.now(),
+      complete: false
+    };
+    this.props.addToDo(newItem);
+    this.setState({ text: "", id: null, complete: false });
+  };
 
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-				<input
-					id='new-todo'
-					onChange={this.handleChange}
-					value={this.state.text}
-				/>
-				<MDBCol xl='3' md='6' className='mx-auto'>
-					<MDBBtn color='info'> Add</MDBBtn>
-				</MDBCol>
-			</form>
-		);
-	}
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          id="new-todo"
+          onChange={this.handleChange}
+          value={this.state.text}
+        />
+        <MDBCol xl="3" md="6" className="mx-auto">
+          <MDBBtn color="info"> Add</MDBBtn>
+        </MDBCol>
+      </form>
+    );
+  }
 }
-export default connect(
-	null,
-	{ addToDo }
-)(NewToDo);
+export default connect(null, { addToDo })(NewToDo);
